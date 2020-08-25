d3a2aabd8c8545046892f8f738a3aa70e587e93d
supinderjit89 <gsupinder1@gmail.com>
2020-08-25 11:37:19 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Checkbox from "@material-ui/core/Checkbox";
+import SearchIcon from "@material-ui/icons/Search";
+//import FormLabel from '@material-ui/core/FormLabel';
+//import FormControl from '@material-ui/core/FormControl';
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+//import FormHelperText from '@material-ui/core/FormHelperText';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import { TextField } from "@material-ui/core";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <div className="App">
+          <div className="grid">
+            <h1>Hello</h1>
+          </div>
+          <hr></hr>
+          <div className="grid1">
+            <TextField width="350px" placeholder="Search here" addonAfter="" />
+            <SearchIcon height="20px" width="20px" />
+          </div>
+        </div>
+        <div className="cbGroup">
+          <div className="grid2">
+            <FormGroup>
+              <FormControlLabel
+                control={<Checkbox name="gilad" />}
+                label="Gilad Gray"
+              />
+                <FormControlLabel
+                control={<Checkbox name="gilad" />}
+                label="Gilad Gray"
+              />
+                <FormControlLabel
+                control={<Checkbox name="gilad" />}
+                label="Gilad Gray"
+              />
+                <FormControlLabel
+                control={<Checkbox name="gilad" />}
+                label="Gilad Gray"
+              />
+            </FormGroup>
+
+          
+          </div>
+        </div>
+      </>
+    );
+  }
 }
-
 export default App;
---
+++ src/checkbox.js
@@ -0,0 +1,11 @@
+const checkboxes=[
+    {
+        title:'A',
+        values:[
+            {name :"check1"},
+            {name  :'check2'},
+        ],
+
+    },
+];
+export default checkboxes;
