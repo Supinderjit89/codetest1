1d19a3c6723025d3f6f7c64940995bd73b03fd31
supinderjit89 <gsupinder1@gmail.com>
2020-08-26 11:50:19 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -9,9 +9,30 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 import "./App.css";
 import { TextField } from "@material-ui/core";
+import data from "./dataItem.json";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: "",
+      dataItem: [...data],
+      searchItem: [],
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.Items = data;
+  }
+  handleChange(e) {
+    this.setState({
+      searchText: e.target.value,
+    });
+  }
+
   render() {
+    const searchItems = this.Items.filter((b) =>
+      b.itemName.includes(this.state.searchText)
+    );
+
     return (
       <>
         <div className="App">
@@ -20,7 +41,12 @@ class App extends Component {
           </div>
           <hr></hr>
           <div className="grid1">
-            <TextField width="350px" placeholder="Search here" addonAfter="" />
+            <TextField
+              width="350px"
+              placeholder="Search here"
+              addonafter=""
+              onChange={this.handleChange}
+            />
             <SearchIcon height="20px" width="20px" />
           </div>
         </div>
@@ -31,22 +57,28 @@ class App extends Component {
                 control={<Checkbox name="gilad" />}
                 label="Gilad Gray"
               />
-                <FormControlLabel
+              <FormControlLabel
                 control={<Checkbox name="gilad" />}
                 label="Gilad Gray"
               />
-                <FormControlLabel
+              <FormControlLabel
                 control={<Checkbox name="gilad" />}
                 label="Gilad Gray"
               />
-                <FormControlLabel
+              <FormControlLabel
                 control={<Checkbox name="gilad" />}
                 label="Gilad Gray"
               />
             </FormGroup>
-
-          
           </div>
+        </div>
+        <div className="cbGroup">
+          {searchItems.map((item) => (
+            <div  className="grid3" key={item.itemName}>
+              <p>{item.itemName}</p>
+              
+            </div>
+          ))}
         </div>
       </>
     );
