e3d60e2ed40900ca8a7df4fa7d9a824ec0b3b312
supinderjit89 <gsupinder1@gmail.com>
2020-08-27 11:58:22 -0700

    changes for v2

--- src/App.js
+++ src/App.js
@@ -100,7 +100,31 @@ class App extends Component {
         </div>
         <div className="footer"></div>
 
+      
+  
+  <Button type="button" className="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</Button>
+
+  
+  <div className="modal fade" id="myModal" role="dialog">
+    <div className="modal-dialog">
     
+      
+      <div className="modal-content">
+        <div className="modal-header">
+          <button type="button" className="close" data-dismiss="modal">&times;</button>
+          <h4 className="modal-title">Modal Header</h4>
+        </div>
+        <div className="modal-body">
+          <p>Some text in the modal.</p>
+        </div>
+        <div className="modal-footer">
+          <Button type="button" className="btn btn-default" data-dismiss="modal">Close</Button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+  
 
         
       </>
